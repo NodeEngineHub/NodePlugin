81bc0959324e6523ce9f6f3483b0744770b49d96
FxMorin <28154542+fxmorin@users.noreply.github.com>
2025-09-20 13:32:05 -0400

    Lombok: Fix lombok errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.9.22")
 
-    implementation("io.freefair.lombok:io.freefair.lombok.gradle.plugin:8.6")
+    implementation("io.freefair.lombok:io.freefair.lombok.gradle.plugin:9.0.0-rc2")
     implementation("net.ltgt.gradle:gradle-errorprone-plugin:4.3.0")
     implementation("com.uber.nullaway:nullaway-annotations:$nullawayVersion")
     api("org.jspecify:jspecify:1.0.0")
